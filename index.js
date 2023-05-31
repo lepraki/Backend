a06626b898988d162157e8dfe8fdc13a29ca7a2d
Gerson Steven <98818664+Stevenjr1028@users.noreply.github.com>
2023-05-31 18:48:27 -0500

    update

--- index.js
+++ index.js
@@ -1,4 +1,52 @@
-const { Server } = require('./Server/server');
+// const { Server } = require('./Server/server');
 
-let s = new Server();
-s.listen();
+// let s = new Server();
+// s.listen();
+
+const express = require('express');
+require('dotenv').config();
+
+// CORS
+const cors = require('cors');
+
+// DB connection
+const { dbConnection } = require('./Database/Config');
+
+// Server
+const app = express();
+
+// Database
+dbConnection();
+
+// CORS options
+const corsOptions = {
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
+
+// CORS
+app.use(cors(corsOptions));
+
+// Public folder
+app.use(express.static('public'));
+
+// Body parser
+app.use(express.json());
+
+// Routes
+// Auth
+app.use('/api/auth',require("./Routes/auth"));
+
+
+// CRUD (Create, Read, Update, Delete)
+// app.use('/api/posts', require('./routes/posts'));
+
+// // chat
+// app.use('/api/chats', require('./routes/chat'));
+
+// Port
+app.listen(process.env.PORT, () => {
+  console.log(`instaDB listening on port ${process.env.PORT}!`);
+});
